8815d4a52ea11b1d9444448bf949213bea988fda
git-rahul23 <jr.rahul07@gmail.com>
2020-07-28 13:49:56 +0530

    Updates

---
+++ precache-manifest.6eecf8ddd237ec46e9a6b0b6ca327919.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a88f844a80746f7dc00f7f5228fd4b3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "621277a8bf76bf36c776",
+    "url": "/static/css/main.db954cd5.chunk.css"
+  },
+  {
+    "revision": "6162b6ac8b56466ccec6",
+    "url": "/static/js/2.90697a33.chunk.js"
+  },
+  {
+    "revision": "b815bed3bedb5a8af7470bb094451139",
+    "url": "/static/js/2.90697a33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "621277a8bf76bf36c776",
+    "url": "/static/js/main.4932f913.chunk.js"
+  },
+  {
+    "revision": "7c95dd441c4b368dd583",
+    "url": "/static/js/runtime-main.b0378703.js"
+  }
+]);
