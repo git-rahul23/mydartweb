958514913d6a194ec91a4b4c9aeda3ead6d617b6
git-rahul23 <jr.rahul07@gmail.com>
2020-07-28 13:59:27 +0530

    Updates

---
+++ precache-manifest.8bc1e7b04bd5cb7957cb8a9bc8dde145.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e356859a8d4ff0b20ca7b3e8be1f1ff",
+    "url": "/mydartweb/index.html"
+  },
+  {
+    "revision": "b9a29407930ebdee354e",
+    "url": "/mydartweb/static/css/main.db954cd5.chunk.css"
+  },
+  {
+    "revision": "6162b6ac8b56466ccec6",
+    "url": "/mydartweb/static/js/2.90697a33.chunk.js"
+  },
+  {
+    "revision": "b815bed3bedb5a8af7470bb094451139",
+    "url": "/mydartweb/static/js/2.90697a33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9a29407930ebdee354e",
+    "url": "/mydartweb/static/js/main.143277c7.chunk.js"
+  },
+  {
+    "revision": "ac02e602ba1e2e9d7405",
+    "url": "/mydartweb/static/js/runtime-main.3daa3728.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6eecf8ddd237ec46e9a6b0b6ca327919.js"
+  "/mydartweb/precache-manifest.8bc1e7b04bd5cb7957cb8a9bc8dde145.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mydartweb/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
